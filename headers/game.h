3c4f67e985dc52538820f8ab728753e5014d4056
Leonardo Moreira <leomoreira.com@gmail.com>
2021-08-11 22:20:21 -0300

    falto os header

---
+++ headers/game.h
@@ -0,0 +1,17 @@
+#ifndef GAME_H_INCLUDED
+#define GAME_H_INCLUDED
+
+/* Functions */
+void gerarTela();//CRIAR
+void changeColorRed();
+void changeColorGreen();
+void changeColorYellow();
+void changeColorPurple();
+void changeColorCyan();
+void changeColorWhite();
+
+int geraCorAleatoria();
+
+int execute();
+/* Functions */
+# endif /* INTERFACE_H_INCLUDED */
--- headers/interface.h
+++ headers/interface.h
@@ -5,6 +5,7 @@
 int mainMenu();    // printa o menu principal
 void creditsMenu(); // printa o menu de créditos
 void gameInformations();    // printa a primeira tela exibida ao iniciar o programa
+void gotoxy(int x, int y); //muda a posição da tela
 /* Functions */
 
 # endif /* INTERFACE_H_INCLUDED */
